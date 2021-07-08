3d64ca2cc4d95e51cf8485e2fa775e7d2e786b84
ldthanh <thanhle421440@gmail.com>
2021-07-08 14:41:52 +0700

    quan ly book

--- src/main/java/dao/BookDAO.java
+++ src/main/java/dao/BookDAO.java
@@ -33,62 +33,6 @@ public class BookDAO {
         this.template = template;
     }
 
-    public List<Book> AllBook() {
-        return template.query("select * from allbook", new RowMapper<Book>() {
-            public Book mapRow(ResultSet rs, int row) throws SQLException {
-                Book b = new Book();
-                b.setId(rs.getInt(1));
-                b.setName(rs.getNString(2));
-                b.setPrice(rs.getFloat(3));
-                b.setPic(rs.getNString(4));
-                b.setPic2(rs.getNString(5));
-                b.setPic3(rs.getNString(6));
-                b.setContent(rs.getNString(7));
-                b.setClassify(rs.getNString(8));
-                return b;
-            }
-        });
-    }
-
-    public void Them(Book b) {
-        String sql = String.format("insert into allbook (name, price, pic, pic2, pic3, content, classify) values('%s','%f','%s','%s','%s','%s','%s')",
-                b.getName(), b.getPrice(), b.getPic(), b.getPic2(), b.getPic3(), b.getContent(), b.getClassify());
-        template.update(sql);
-    }
-
-    public void Them_NoImage(Book b) {
-        String sql = String.format("insert into allbook (name, price, content, classify) values('%s','%f','%s','%s')",
-                b.getName(), b.getPrice(), b.getContent(), b.getClassify());
-        template.update(sql);
-    }
-
-    public int CapNhat(Book b) {
-        String sql = String.format("update allbook set price='%f' ,pic='%s', pic2='%s', pic3='%s', content='%s',classify='%s' where name=%s",
-                b.getPrice(), b.getPic(), b.getPic2(), b.getPic3(), b.getContent(), b.getClassify(), b.getName());
-        return template.update(sql);
-    }
-
-    public int CapNhat_NoImage(Book b) {
-        String sql = String.format("update allbook set name='%s' ,price='%f', content='%s' where id ='%d' ",
-               b.getName(), b.getPrice(), b.getContent(),b.getId() );
-        return template.update(sql);
-    }
-
-    public int Xoa( String id) {
-        String sql = "delete from allbook where id = ?";
-        return template.update(sql, id);
-    }
-
-    public Book DetailBook(int id) {
-        String sql = "select * from allbook where id = ?";
-        return template.queryForObject(sql, new Object[]{id}, new BeanPropertyRowMapper<>(Book.class));
-    }
-
-    public List<Book> Search_Book(String name) {
-        String sql = "select * from allbook where name like ?";
-        return template.query(sql, new Object[]{"%" + name + "%"}, new BeanPropertyRowMapper<>(Book.class));
-    }
-
     // gio hang
     public void ThemCart(Cart cart) {
 //        Cart id = searchCart(cart.getId());
